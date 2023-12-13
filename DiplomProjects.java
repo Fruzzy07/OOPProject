3b75ae2ac2b0cc8abe8279779abb6234d23d0206
Yernur Shapkat <113994667+Fruzzy07@users.noreply.github.com>
2023-12-13 19:16:36 +0600

    DiplomProjects.java

--- DiplomProjects.java
+++ DiplomProjects.java
@@ -2,7 +2,25 @@ package projects;
 
 import java.util.Vector;
 
+
 public class DiplomProjects {
-	public static Vector<ResearchProject> = new Vector<ResearchProject>();
-	
+    private String title;
+    private Vector<ResearchPaper> publishedPapers = new Vector<ResearchPaper>();
+
+    public DiplomProjects(String title) {
+        this.title = title;
+    }
+
+    public void addResearchPaper(ResearchPaper paper) {
+        publishedPapers.add(paper);
+    }
+
+    public void viewResearchPapers() {
+        System.out.println("Research Papers for Diploma Project: " + title);
+        for (ResearchPaper paper : publishedPapers) {
+            System.out.println("Title: " + paper.title);
+            System.out.println("Authors: " + paper.author);
+            System.out.println("Pages: " + paper.pages);
+        }
+    }
 }
